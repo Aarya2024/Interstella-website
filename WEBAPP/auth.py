6901f8d9dc1e801a8de07d54d652721c5b25d241
Pratham4040 <prathameshpaliwal19@gmail.com>
2024-07-15 19:51:02 +0530

    updated frontend elements and routed quit form

--- WEBAPP/auth.py
+++ WEBAPP/auth.py
@@ -2,5 +2,5 @@ from flask import Blueprint ,render_template, request , flash,redirect,url_for
 auth = Blueprint('auth',__name__)
 @auth.route('/quiz',methods =['POST','GET'])
 def quiz():
-      
+
     return render_template("quiz.html") 
