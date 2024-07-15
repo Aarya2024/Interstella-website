7c46b2ed46ea7574958d417552df87533ec062a1
Pratham4040 <prathameshpaliwal19@gmail.com>
2024-07-15 18:37:25 +0530

    routed quiz

--- WEBAPP/auth.py
+++ WEBAPP/auth.py
@@ -1,8 +1,6 @@
-from flask_login import login_user, login_required, logout_user, current_user
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask import Blueprint ,render_template, request , flash,redirect,url_for
 auth = Blueprint('auth',__name__)
-@auth.route('/login',methods =['POST','GET'])
-def login():
-        
-    return render_template("index.html") 
+@auth.route('/quiz',methods =['POST','GET'])
+def quiz():
+      
+    return render_template("quiz.html") 
