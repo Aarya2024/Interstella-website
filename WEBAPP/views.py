0ea9e9b2386143aaabdc37c983fd001a9d963c78
Pratham4040 <prathameshpaliwal19@gmail.com>
2024-07-16 18:27:17 +0530

    connected db to the page

--- WEBAPP/views.py
+++ WEBAPP/views.py
@@ -1,5 +1,7 @@
-from flask import Blueprint ,render_template
+from flask import Blueprint,jsonify, render_template,request,flash
 from  wtforms import FileField
+from .models import User
+from . import db
 # from flask_wtf import FlaskForm
 import os, shutil
 views = Blueprint('views',__name__)
@@ -7,5 +9,23 @@ views = Blueprint('views',__name__)
 def home():
     return render_template("index.html")
 @views.route('/quiz', methods=['POST','GET'])
-def quiz():    
+def quiz():
+    if request.method == 'POST':
+        #fetching
+        studentName=request.form.get('fullName')
+        schoolName=request.form.get('school')
+        studentAge=request.form.get('age')
+        studentContactNumber=request.form.get('contact')
+        emailAddress=request.form.get('email')
+        studentSelectedLocation=request.form.get('location')
+        
+        new_student=User(studentName=studentName,schoolName=schoolName,studentAge=studentAge,studentContactNumber=studentContactNumber,emailAddress=emailAddress,studentSelectedLocation=studentSelectedLocation)
+        db.session.add(new_student)
+        db.session.commit()
+        return render_template("index.html")
+
+
+
+        
+
     return render_template("quiz.html") 
